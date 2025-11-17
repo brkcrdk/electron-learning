4bfb79a82c594da8e18a1c21fb4058a414d3cc41
Burak <burak@90pixel.com>
2025-11-17 22:53:27 +0300

    Enhance Vite environment type declarations with additional comments for clarity and guidance on usage in TypeScript projects.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,10 @@
 /// <reference types="vite/client" />
 
-// Electron Forge Vite plugin globals
+/**
+ * typescript kurulumu sırasında yapılan değerlerin ts tarafından önerilmesi için
+ * bu şekilde vite global configine eklenmesi gerekiyor.
+ *
+ * @see https://www.electronforge.io/config/plugins/webpack#main-process
+ */
 declare const MAIN_WINDOW_VITE_DEV_SERVER_URL: string;
 declare const MAIN_WINDOW_VITE_NAME: string;
