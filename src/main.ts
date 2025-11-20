4c41dce94a29d65dfa4b3c8f9bde9f8779e25fa9
Burak <burak@90pixel.com>
2025-11-20 22:54:39 +0300

    Add minimum window size constraints for main application window

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,8 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
+    minWidth: 800,
+    minHeight: 600,
     title: 'Electron Learning',
     center: true,
     webPreferences: {
