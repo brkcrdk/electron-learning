78e0476d0905627f241e4ed28a4bebfdde75bcc7
Burak <burak@90pixel.com>
2025-11-22 17:30:24 +0300

    Add API exposer handlers for renderer process
    - Introduced a new `apiExposerHandlers` object to expose API functions to the renderer via `preload.ts`.
    - Added documentation to clarify the purpose and type definitions of the exposed API, ensuring better type safety for `window.electronAPI` in the UI.

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,6 +4,12 @@ export function registerApiHandlers() {
   createUserHandler();
 }
 
+/**
+ * Burada tanımlanan api functionları `renderer` tarafına `preload.ts` ile expose edilir.
+ *
+ * `as const` kullanılarak tip tanımlaması da oluşuturulur ve böylece `window.electronAPI` objesinin tipi sabitlenir.
+ * Bu sayede `ui` tarafında `window.electronAPI` objesinin tip tanımlaması bu objeden oluşuturulur.
+ */
 export const apiExposerHandlers = {
   createUserEvent,
 } as const;
