91c87939705244db3c3d2aa215315019644c47d8
Burak <burak@90pixel.com>
2025-11-25 17:42:04 +0300

    Translate login form labels and button text to Turkish for localization

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -10,20 +10,20 @@ function RouteComponent() {
   return (
     <section className="flex h-screen w-screen items-center justify-center">
       <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-        <legend className="fieldset-legend">Login</legend>
+        <legend className="fieldset-legend">Giriş Yap</legend>
 
-        <label className="label">Email</label>
+        <label className="label">E-posta</label>
         <input
           type="email"
           className="input"
           placeholder="example@example.com"
         />
 
-        <label className="label">Password</label>
+        <label className="label">Şifre</label>
 
         <PasswordInput inputProps={{ placeholder: '********' }} />
 
-        <button className="btn btn-neutral mt-4">Login</button>
+        <button className="btn btn-neutral mt-4">Giriş Yap</button>
       </fieldset>
     </section>
   );
