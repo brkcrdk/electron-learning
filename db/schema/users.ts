28911952d7129bf50fb22203b2e45f035d2ee04b
Burak <burak@90pixel.com>
2025-11-27 16:47:29 +0300

    update: user schema.

--- db/schema/users.ts
+++ db/schema/users.ts
@@ -5,7 +5,20 @@ export const users = sqliteTable('users', {
   id: integer('id').primaryKey({ autoIncrement: true }),
   name: text('name').notNull(),
   email: text('email').notNull().unique(),
+  password: text('password').notNull(),
+  roles: text('roles', { enum: ['admin', 'user'] })
+    .notNull()
+    .default('user'),
+  status: text('status', { enum: ['active', 'passive'] })
+    .notNull()
+    .default('active'),
   createdAt: integer('created_at', { mode: 'timestamp' })
     .notNull()
     .default(sql`CURRENT_TIMESTAMP`),
+  updatedAt: integer('updated_at', { mode: 'timestamp' })
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+  lastLoginAt: integer('last_login_at', { mode: 'timestamp' }),
 });
+
+export type User = typeof users.$inferSelect;
