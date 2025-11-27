3c93ec692d60da258ced6815983e7afd15c1385b
Burak <burak@90pixel.com>
2025-11-27 16:52:13 +0300

    update: extend user roles in schema to include 'super-admin'

--- db/schema/users.ts
+++ db/schema/users.ts
@@ -6,7 +6,7 @@ export const users = sqliteTable('users', {
   name: text('name').notNull(),
   email: text('email').notNull().unique(),
   password: text('password').notNull(),
-  roles: text('roles', { enum: ['admin', 'user'] })
+  roles: text('roles', { enum: ['super-admin', 'admin', 'user'] })
     .notNull()
     .default('user'),
   status: text('status', { enum: ['active', 'passive'] })
