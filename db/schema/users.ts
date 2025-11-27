6d1f07e1176a1ddf624bdc7f208f97f021ededaa
Burak <burak@90pixel.com>
2025-11-27 22:37:29 +0300

    feat: add index on roles in users schema
    - Enhanced the users schema by adding an index on the roles field for improved query performance.
    - Reformatted the schema definition for better readability.

--- db/schema/users.ts
+++ db/schema/users.ts
@@ -1,24 +1,28 @@
 import { sql } from 'drizzle-orm';
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { integer, sqliteTable, text, index } from 'drizzle-orm/sqlite-core';
 
-export const users = sqliteTable('users', {
-  id: integer('id').primaryKey({ autoIncrement: true }),
-  name: text('name').notNull(),
-  email: text('email').notNull().unique(),
-  password: text('password').notNull(),
-  roles: text('roles', { enum: ['super-admin', 'admin', 'user'] })
-    .notNull()
-    .default('user'),
-  status: text('status', { enum: ['active', 'passive'] })
-    .notNull()
-    .default('active'),
-  createdAt: integer('created_at', { mode: 'timestamp' })
-    .notNull()
-    .default(sql`CURRENT_TIMESTAMP`),
-  updatedAt: integer('updated_at', { mode: 'timestamp' })
-    .notNull()
-    .default(sql`CURRENT_TIMESTAMP`),
-  lastLoginAt: integer('last_login_at', { mode: 'timestamp' }),
-});
+export const users = sqliteTable(
+  'users',
+  {
+    id: integer('id').primaryKey({ autoIncrement: true }),
+    name: text('name').notNull(),
+    email: text('email').notNull().unique(),
+    password: text('password').notNull(),
+    roles: text('roles', { enum: ['super-admin', 'admin', 'user'] })
+      .notNull()
+      .default('user'),
+    status: text('status', { enum: ['active', 'passive'] })
+      .notNull()
+      .default('active'),
+    createdAt: integer('created_at', { mode: 'timestamp' })
+      .notNull()
+      .default(sql`CURRENT_TIMESTAMP`),
+    updatedAt: integer('updated_at', { mode: 'timestamp' })
+      .notNull()
+      .default(sql`CURRENT_TIMESTAMP`),
+    lastLoginAt: integer('last_login_at', { mode: 'timestamp' }),
+  },
+  table => [index('roles_idx').on(table.roles)]
+);
 
 export type User = typeof users.$inferSelect;
