e87d3644c5dc49f768b86018c7c105384fac8ba1
Burak <burak@90pixel.com>
2025-11-29 12:49:11 +0300

    refactor: simplify TitleBar component and add webkit-no-drag utility
    - Removed unnecessary class names and streamlined the TitleBar component for better readability.
    - Introduced a new CSS utility for webkit-no-drag to prevent dragging in specific areas, enhancing user interaction.
    - Updated the layout to replace the avatar span with a more descriptive div for clarity.

--- app/layout/title-bar.tsx
+++ app/layout/title-bar.tsx
@@ -1,23 +1,16 @@
-import cn from '../utils/cn';
-
 function TitleBar() {
   return (
-    <div
-      className={cn(
-        'bg-base-100 relative flex h-12 w-full items-center justify-between px-4',
-        'before:webkit-draggable before:absolute before:inset-0 before:size-full before:content-[""]'
-      )}
-    >
+    <div className="bg-base-100 webkit-draggable relative flex h-12 w-full items-center justify-between px-4">
       <div />
-      <nav className="z-10 flex items-center gap-2">
+      <nav className="webkit-no-drag z-10 flex items-center gap-2">
         <div className="flex">
           <button className="btn btn-xs">Ileri</button>
           <button className="btn btn-xs">Geri</button>
         </div>
-        {/* <input className="input input-sm input-bordered min-w-sm w-full" /> */}
+
         <button className="btn btn-xs">Ara</button>
       </nav>
-      <span>Avatar</span>
+      <div className="webkit-no-drag">avatar area</div>
     </div>
   );
 }
