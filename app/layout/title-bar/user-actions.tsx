dad78497c36ba3920be69fbc4d62fe7b327e6e8a
Burak <burak@90pixel.com>
2025-11-30 18:28:56 +0300

    feat: enhance UserActions component with avatar and chevron-down icon
    - Updated the UserActions component to include an avatar and a chevron-down icon in the dropdown trigger, improving the visual representation and user interaction.

--- app/layout/title-bar/user-actions.tsx
+++ app/layout/title-bar/user-actions.tsx
@@ -1,8 +1,25 @@
 import Dropdown from '../../components/ui/dropdown';
+import Icon from '../../components/ui/icon';
 
 function UserActions() {
   return (
     <Dropdown
+      triggerProps={{
+        className: 'px-1',
+        children: (
+          <>
+            <div className="avatar">
+              <div className="size-6 rounded-md">
+                <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
+              </div>
+            </div>
+            <Icon
+              name="chevron-down"
+              className="size-4"
+            />
+          </>
+        ),
+      }}
       dropdownItems={[
         {
           itemType: 'link',
