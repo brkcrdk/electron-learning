ccc09e814cae900d39442a050ee13ae63c6479d2
Burak <burak@90pixel.com>
2025-11-30 22:32:59 +0300

    feat: integrate sidebar-expand icon and refactor Header component
    - Added a new sidebar-expand icon to the icon list for enhanced navigation options.
    - Refactored the Header component to utilize the new Icon component for rendering the sidebar-expand icon, improving code maintainability and readability.
    - Updated Layout component to include the Header, streamlining the overall structure.

--- app/components/ui/icon/icon-list.ts
+++ app/components/ui/icon/icon-list.ts
@@ -1,6 +1,7 @@
 import { HiShieldCheck } from 'react-icons/hi2';
 import { LuMoon, LuSun, LuArrowLeft, LuArrowRight, LuChevronDown, LuX } from 'react-icons/lu';
 import { RxEyeClosed, RxEyeOpen } from 'react-icons/rx';
+import { TbLayoutSidebarLeftExpand } from 'react-icons/tb';
 
 export const iconList = [
   { label: 'eye-open', icon: RxEyeOpen },
@@ -12,6 +13,7 @@ export const iconList = [
   { label: 'arrow-right', icon: LuArrowRight },
   { label: 'chevron-down', icon: LuChevronDown },
   { label: 'close', icon: LuX },
+  { label: 'sidebar-expand', icon: TbLayoutSidebarLeftExpand },
 ] as const;
 
 export type IconListProps = (typeof iconList)[number]['label'];
--- app/layout/index.tsx
+++ app/layout/index.tsx
@@ -1,3 +1,4 @@
+import Header from './header';
 import TitleBar from './title-bar';
 import Modal from '../components/ui/modal';
 
@@ -14,28 +15,7 @@ function Layout({ children }: PropsWithChildren) {
           className="drawer-toggle"
         />
         <div className="drawer-content bg-base-300">
-          <nav className="navbar bg-base-300 border-base-200 w-full border-b">
-            <label
-              htmlFor="my-drawer-4"
-              aria-label="open sidebar"
-              className="btn btn-square btn-ghost"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                strokeLinejoin="round"
-                strokeLinecap="round"
-                strokeWidth="2"
-                fill="none"
-                stroke="currentColor"
-                className="my-1.5 inline-block size-4"
-              >
-                <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path>
-                <path d="M9 4v16"></path>
-                <path d="M14 10l2 2l-2 2"></path>
-              </svg>
-            </label>
-          </nav>
+          <Header />
           <div className="p-4">
             {children}
             <Modal
