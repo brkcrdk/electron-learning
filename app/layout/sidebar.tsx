b9e3e055825ab3034acdbe5c2f02faf0a15d0bda
Burak <burak@90pixel.com>
2025-11-30 22:42:00 +0300

    feat: update ESLint configuration and refactor Sidebar component
    - Added ignores for specific directories in the ESLint configuration to streamline linting processes.
    - Refactored Sidebar component to improve accessibility by replacing self-closing tags with proper syntax for better compatibility and readability.

--- app/layout/sidebar.tsx
+++ app/layout/sidebar.tsx
@@ -5,7 +5,7 @@ function Sidebar() {
         htmlFor="my-drawer-4"
         aria-label="close sidebar"
         className="drawer-overlay"
-      ></label>
+      />
       <div className="bg-base-200 border-base-100 is-drawer-close:w-14 is-drawer-open:w-64 flex min-h-full flex-col items-start">
         <ul className="menu w-full grow">
           <li>
@@ -23,8 +23,8 @@ function Sidebar() {
                 stroke="currentColor"
                 className="my-1.5 inline-block size-4"
               >
-                <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path>
-                <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
+                <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
               </svg>
               <span className="is-drawer-close:hidden">Homepage</span>
             </button>
@@ -45,18 +45,18 @@ function Sidebar() {
                 stroke="currentColor"
                 className="my-1.5 inline-block size-4"
               >
-                <path d="M20 7h-9"></path>
-                <path d="M14 17H5"></path>
+                <path d="M20 7h-9" />
+                <path d="M14 17H5" />
                 <circle
                   cx="17"
                   cy="17"
                   r="3"
-                ></circle>
+                />
                 <circle
                   cx="7"
                   cy="7"
                   r="3"
-                ></circle>
+                />
               </svg>
               <span className="is-drawer-close:hidden">Settings</span>
             </button>
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -41,4 +41,7 @@ export default [
       ],
     },
   },
+  {
+    ignores: ['node_modules/**', '.vite/**', 'out/**', 'build/**', 'drizzle/**'],
+  },
 ];
