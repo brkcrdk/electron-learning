011e2ffd5ed3fb619299edc2decb1fda2fadb2a1
Burak <burak@90pixel.com>
2025-11-30 18:28:51 +0300

    refactor: simplify class name utility by removing custom twMerge implementation
    - Removed the custom implementation of twMerge in favor of directly importing it from 'tailwind-merge', streamlining the utility function for better maintainability.

--- app/utils/cn.ts
+++ app/utils/cn.ts
@@ -1,21 +1,5 @@
 import { cx } from 'class-variance-authority';
-import { type ClassNameValue, extendTailwindMerge } from 'tailwind-merge';
-
-/**
- * NOTE: TwMerge stillendirmeleri mergelerken sadece sadece tw stillendirmelerini dikkate alıyor.
- * Biz de stillendirmeleri mergelerken başında `cs-` prefixi olmadan yokmuş gibi davranmasını sağlıyoruz.
- * Böylece hem classlarımızı koruyoruz hem de mergelenirnek doğru çalışmasını sağlıyoruz.
- */
-const twMerge = extendTailwindMerge({
-  experimentalParseClassName: ({ className, parseClassName }) => {
-    const parsed = parseClassName(className);
-
-    return {
-      ...parsed,
-      baseClassName: className.startsWith('cs-') ? parsed.baseClassName.split('cs-')[1] : parsed.baseClassName,
-    };
-  },
-});
+import { type ClassNameValue, twMerge } from 'tailwind-merge';
 
 function cn(...inputs: ClassNameValue[]) {
   return twMerge(cx(inputs));
