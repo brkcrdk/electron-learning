55a770460572160b19db2cae9b77229857650bc8
Burak <burak@90pixel.com>
2025-12-01 14:48:54 +0300

    feat: export TableActionsProps type for improved type safety
    - Added export for TableActionsProps type in the Table component file to enhance type safety and organization.
    - This change supports better integration with the TableActions component and improves overall code maintainability.

--- app/components/ui/Table/index.tsx
+++ app/components/ui/Table/index.tsx
@@ -1,9 +1,13 @@
 import MainTable, { type TableProps } from './main-table';
 import TableLoader from './table-loader';
 
+import type { TableActionsProps } from './table-types';
+
 function Table<T>({ isLoading, ...props }: TableProps<T>) {
   if (isLoading) return <TableLoader />;
 
   return <MainTable {...props} />;
 }
 export default Table;
+
+export type { TableActionsProps };
