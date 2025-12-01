40d0e51a7dc2112e7d347e021f60c6b99d9f97d4
Burak <burak@90pixel.com>
2025-12-01 14:46:57 +0300

    refactor: reorganize TableSearchBar component into a new directory structure
    - Removed the old TableSearchBar implementation and created a new version in the table-header directory for better organization.
    - Updated TableHeader to utilize the new TableSearchBar component, maintaining the same functionality with improved structure.

---
+++ app/components/ui/Table/table-header/index.tsx
@@ -0,0 +1,27 @@
+import TableActions from './table-actions';
+import TableSearchBar, { type TableSearchProps } from './table-search-bar';
+
+import type { TableActionsProps } from '../table-types';
+
+interface TableHeaderProps {
+  tableTitle?: string;
+  searchProps?: TableSearchProps;
+  tableActions?: TableActionsProps[];
+}
+
+function TableHeader({ tableTitle, searchProps, tableActions }: TableHeaderProps) {
+  return (
+    <header className="flex h-fit items-center justify-between gap-4">
+      {tableTitle && <h3 className="text-xl font-medium">{tableTitle}</h3>}
+      {searchProps && (
+        <TableSearchBar
+          value={searchProps.value}
+          placeholder={searchProps.placeholder}
+          onSearch={searchProps.onSearch}
+        />
+      )}
+      {tableActions && tableActions.length > 0 ? <TableActions actions={tableActions} /> : null}
+    </header>
+  );
+}
+export default TableHeader;
