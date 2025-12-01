6f54904e2448a57b2f0eb742c595e1c430164e8c
Burak <burak@90pixel.com>
2025-12-01 14:46:41 +0300

    fix: correct loading state handling in Table component
    - Updated Table component to properly utilize the isLoading prop for conditional rendering.
    - Ensured that TableLoader is displayed when loading, and MainTable is rendered when not loading.

--- app/components/ui/Table/index.tsx
+++ app/components/ui/Table/index.tsx
@@ -1,10 +1,9 @@
 import MainTable, { type TableProps } from './main-table';
 import TableLoader from './table-loader';
 
-function Table<T>({ ...props }: TableProps<T>) {
-  // if (isLoading) return <TableLoader />;
+function Table<T>({ isLoading, ...props }: TableProps<T>) {
+  if (isLoading) return <TableLoader />;
 
-  // return <MainTable {...props} />;
-  return <TableLoader />;
+  return <MainTable {...props} />;
 }
 export default Table;
