2b8fe97027a3b5be87edc7f6af3b296c2cd788b1
Burak <burak@90pixel.com>
2025-12-07 23:20:57 +0300

    feat: add RelativeDateCell component and update column definitions for date handling
    - Introduced a new RelativeDateCell component to display dates in a user-friendly format, showing relative time and providing tooltips for exact timestamps.
    - Updated the useColumns hook to utilize the RelativeDateCell for 'Oluşturulma Tarihi' and 'Son Giriş Tarihi' columns, enhancing date presentation in the user list.

---
+++ app/components/table-cells/relative-date-cell.tsx
@@ -0,0 +1,29 @@
+import dayjs from '../../utils/dayjs';
+
+interface Props {
+  date: Date;
+}
+
+/**
+ * Tablolarda kullanılan tarih bilgilerini görüntülemek için kullanılan bir hücre componentidir.
+ * Bu bilgiler genelde spesifik bir tarihi ifade ettiği için ilk bakışta ne zaman olduğunu anlamak
+ * için hesaplama gerektiği için bunun yerine direk olarak kaç zaman önce olduğunu gösteren bir süre
+ * belirtilir. Tam tarihi belirtmek için de tooltip kullanılır ve bu şekilde her iki şekilde de kullanıcıya
+ * arzulanan bilgiler sunulabilir.
+ *
+ * @example
+ * Örnek girdi: 2025-04-15T12:00:00.000Z
+ * Örnek çıktı: 1 saat önce, 4 ay önce, 1 yıl önce
+ * Örnek tooltip çıktısı: 15 Nisan 2025 veya 15 Nisan 2025 12:00:00
+ */
+function RelativeDateCell({ date }: Props) {
+  return (
+    <div
+      className="tooltip"
+      data-tip={dayjs(date).format('DD.MM.YYYY HH:mm')}
+    >
+      {dayjs(date).fromNow()}
+    </div>
+  );
+}
+export default RelativeDateCell;
---
+++ app/routes/_main/users/hooks/use-columns.tsx
@@ -0,0 +1,44 @@
+import RelativeDateCell from '../../../../components/table-cells/relative-date-cell';
+
+import type { User } from '../../../../../db/schema';
+import type { ColumnDef } from '@tanstack/react-table';
+
+function useColumns(): ColumnDef<User>[] {
+  return [
+    {
+      header: 'Adı',
+      accessorKey: 'name',
+    },
+    {
+      header: 'Email',
+      accessorKey: 'email',
+    },
+    {
+      header: 'Rol',
+      accessorKey: 'roles',
+    },
+    {
+      header: 'Durum',
+      accessorKey: 'status',
+    },
+    {
+      header: 'Oluşturulma Tarihi',
+      accessorKey: 'createdAt',
+      cell: info => info.getValue(),
+      accessorFn: ({ createdAt }) => {
+        return <RelativeDateCell date={createdAt} />;
+      },
+    },
+    {
+      header: 'Son Giriş Tarihi',
+      accessorKey: 'lastLoginAt',
+      cell: info => info.getValue(),
+      accessorFn: ({ lastLoginAt }) => {
+        if (!lastLoginAt) return null;
+        return <RelativeDateCell date={lastLoginAt} />;
+      },
+    },
+  ];
+}
+
+export default useColumns;
