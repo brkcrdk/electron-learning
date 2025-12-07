9fc45cd21b66fe45d84f6ffa6bfbfddf2bd6bbc9
Burak <burak@90pixel.com>
2025-12-08 00:27:19 +0300

    feat: update dayjs locale import for Turkish support
    - Replaced the direct string locale assignment with an import from 'dayjs/locale/tr' to enhance clarity and maintainability.
    - Ensured proper integration of the Turkish locale in the dayjs configuration.

--- app/utils/dayjs.ts
+++ app/utils/dayjs.ts
@@ -1,4 +1,5 @@
 import dayjs from 'dayjs';
+import tr from 'dayjs/locale/tr';
 import calendar from 'dayjs/plugin/calendar';
 import duration from 'dayjs/plugin/duration';
 import localizedFormat from 'dayjs/plugin/localizedFormat';
@@ -10,6 +11,6 @@ dayjs.extend(utc);
 dayjs.extend(relativeTime);
 dayjs.extend(localizedFormat);
 dayjs.extend(calendar);
-dayjs.locale('tr');
+dayjs.locale(tr);
 
 export default dayjs;
