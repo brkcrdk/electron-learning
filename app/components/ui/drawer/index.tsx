0770f16dd4f7419ccda780cac643408616255e8c
Burak <burak@90pixel.com>
2025-12-08 16:57:23 +0300

    refactor: simplify Drawer component by removing close icon prop and integrating DrawerContext
    - Removed the `hasCloseIcon` prop from the Drawer component to streamline its API.
    - Integrated DrawerContextProvider for improved state management and flexibility.
    - Added DrawerHeader component for enhanced visual structure and usability.

--- app/components/ui/drawer/index.tsx
+++ app/components/ui/drawer/index.tsx
@@ -3,60 +3,45 @@ import type { LabelHTMLAttributes, PropsWithChildren } from 'react';
 import cn from '@app/utils/cn';
 
 import CloseBtn from './close-btn';
-import Icon from '../icon';
+import { DrawerContextProvider } from './drawer-context';
+import DrawerHeader from './header';
 
 interface Props extends PropsWithChildren {
   triggerProps: LabelHTMLAttributes<HTMLLabelElement>;
   drawerId: string;
-  /**
-   * Drawer headerında kapatma iconunu render edip etmeyeceğimizi belirten proptur
-   * @defaultValue `true`
-   */
-  hasCloseIcon?: boolean;
 }
 
-function Drawer({ children, triggerProps, hasCloseIcon = true, drawerId }: Props) {
+function Drawer({ children, triggerProps, drawerId }: Props) {
   return (
-    <div className="drawer drawer-end">
-      <input
-        id={drawerId}
-        type="checkbox"
-        className="drawer-toggle"
-      />
-      <div className="drawer-content">
-        <label
-          htmlFor={drawerId}
-          {...triggerProps}
-          className={cn('btn btn-primary', triggerProps?.className)}
+    <DrawerContextProvider drawerId={drawerId}>
+      <div className="drawer drawer-end">
+        <input
+          id={drawerId}
+          type="checkbox"
+          className="drawer-toggle"
         />
-      </div>
+        <div className="drawer-content">
+          <label
+            htmlFor={drawerId}
+            {...triggerProps}
+            className={cn('btn btn-primary', triggerProps?.className)}
+          />
+        </div>
 
-      <div className="drawer-side webkit-no-draggable z-50">
-        <label
-          htmlFor={drawerId}
-          aria-label="close sidebar"
-          className="drawer-overlay"
-        />
-        <div className="menu bg-base-200 max-w-120 min-h-full w-1/2 p-4">
-          {hasCloseIcon && (
-            <label
-              htmlFor={drawerId}
-              aria-label="close drawer"
-              className="btn btn-square btn-ghost absolute right-4 top-2"
-            >
-              <Icon
-                name="close"
-                className="size-4"
-              />
-            </label>
-          )}
-          {children}
+        <div className="drawer-side webkit-no-draggable z-50">
+          <label
+            htmlFor={drawerId}
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          />
+          <div className="menu bg-base-200 max-w-120 min-h-full w-1/2 p-4">{children}</div>
         </div>
       </div>
-    </div>
+    </DrawerContextProvider>
   );
 }
 
 Drawer.CloseBtn = CloseBtn;
+Drawer.Header = DrawerHeader;
 
 export default Drawer;
