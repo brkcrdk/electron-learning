87eedf3249423e2de3324e7092162848f8c5c2c1
Burak <burak@90pixel.com>
2025-12-10 00:08:35 +0300

    feat: add CellContent and Content components for enhanced table header functionality
    - Introduced CellContent component to manage sortable table header cells, allowing for sorting interactions.
    - Added Content component to display header content along with sorting icons based on the current sort direction.
    - Updated RowItem component to integrate new CellContent, improving the overall table header structure and user experience.

---
+++ app/components/data-table/table-head/row-item/index.tsx
@@ -0,0 +1,38 @@
+import { type Header, flexRender } from '@tanstack/react-table';
+
+import Table from '@app/components/ui/table';
+import cd from '@app/utils/cd';
+
+import CellContent from './cell-content';
+import type { SortingChangeStateProps } from '../../main-table';
+
+interface Props<T> {
+  header: Header<T, unknown>;
+  onSortingChange?: (e: SortingChangeStateProps) => void;
+}
+
+function RowItem<T>({ header, onSortingChange }: Props<T>) {
+  const { meta } = header.column.columnDef;
+
+  return (
+    <Table.Head
+      key={header.id}
+      data-sortable={cd(header.column.getCanSort())}
+      data-sorted={cd(header.column.getIsSorted())}
+      data-centered={cd(meta?.centeredColumn)}
+      // Tablomuzda sadece sola doğru sabitleme yaptığımız için left olup olmadığını kontrol ediyoruz.
+      data-pinned={cd(header.column.getIsPinned() === 'left')}
+      colSpan={header.colSpan}
+      className="group/th"
+    >
+      <CellContent
+        headerId={header.id}
+        content={flexRender(header.column.columnDef.header, header.getContext())}
+        onSortingChange={onSortingChange}
+        columnIsSorted={header.column.getIsSorted()}
+        isSortable={header.column.getCanSort()}
+      />
+    </Table.Head>
+  );
+}
+export default RowItem;
