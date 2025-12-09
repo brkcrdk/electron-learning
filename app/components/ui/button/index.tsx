ba57d8c55e555779c108c01cfc530b37b98a34b2
Burak <burak@90pixel.com>
2025-12-09 15:40:06 +0300

    feat: enhance Button component with loading state support
    - Added isLoading prop to the Button component to display a Spinner during loading states.
    - Updated Button rendering to conditionally show the Spinner and children elements for improved user experience.

--- app/components/ui/button/index.tsx
+++ app/components/ui/button/index.tsx
@@ -5,22 +5,26 @@ import { Slot } from 'radix-ui';
 import cn from '@app/utils/cn';
 
 import buttonVariants, { type SizeVariantTypes, type StyleVariantTypes } from './variants';
+import Spinner from '../spinner';
 
 interface Props extends ComponentProps<'button'> {
   asChild?: boolean;
   variant?: StyleVariantTypes;
   size?: SizeVariantTypes;
+  isLoading?: boolean;
 }
 
-function Button({ className, variant, size, asChild = false, ...props }: Props) {
+function Button({ className, variant, size, asChild = false, isLoading = false, ...props }: Props) {
   const Comp = asChild ? Slot.Root : 'button';
 
   return (
     <Comp
       data-slot="button"
       className={cn(buttonVariants({ variant, size, className }))}
-      {...props}
-    />
+    >
+      {isLoading && <Spinner />}
+      {props.children}
+    </Comp>
   );
 }
 
