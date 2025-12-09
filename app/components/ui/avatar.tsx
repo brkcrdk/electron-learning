c2bd4e89ecac6adc983551c76a63acf8e07f99d5
Burak <burak@90pixel.com>
2025-12-09 22:45:25 +0300

    feat: add Avatar component for user profile representation
    - Introduced a new Avatar component to display user initials or images with fallback options.
    - Updated UserActions component to utilize the new Avatar component within a Dropdown for enhanced user interaction.
    - Improved Dropdown structure by integrating Button for better accessibility and styling.

---
+++ app/components/ui/avatar.tsx
@@ -0,0 +1,87 @@
+import { type HTMLAttributes, type ImgHTMLAttributes, useState } from 'react';
+
+import { Avatar as AvatarPrimitive } from 'radix-ui';
+
+import cn from '@app/utils/cn';
+
+/**
+ * Avatarda verilen `name` propuna ait değeri avatarın yanında render edip etmek istediğimizi
+ * ifade eden proptur. Eğer render etmek istersek `hideAvatarName` propunu `false` yapmamız gerekiyor.
+ * Bunun yanında ismi render ettiğimiz span elementinin propsunu `nameElementProps` propu ile veriyoruz.
+ *
+ * @defaultValue `{ hideAvatarName: true }`
+ */
+type AvatarNameProps =
+  | {
+      hideAvatarName: false;
+      nameElementProps?: HTMLAttributes<HTMLSpanElement>;
+    }
+  | {
+      hideAvatarName: true;
+    };
+
+const getFirstLetters = (name: string) => {
+  if (!name) return '';
+  return name
+    .split(' ')
+    .map(word => word.charAt(0).toLocaleUpperCase())
+    .join('');
+};
+const getRandomColor = () => {
+  const colors = ['bg-amber-500', 'bg-indigo-500', 'bg-sky-500', 'bg-emerald-500', 'bg-cyan-500', 'bg-fuchsia-500'];
+  const randomIndex = Math.floor(Math.random() * colors.length - 1);
+  return colors[randomIndex === -1 ? 0 : randomIndex];
+};
+
+interface Props {
+  name: string;
+  avatarRootProps?: AvatarPrimitive.AvatarProps;
+  avatarImageProps?: Omit<ImgHTMLAttributes<HTMLImageElement>, 'width' | 'height' | 'alt'>;
+  avatarFallbackProps?: AvatarPrimitive.AvatarFallbackProps;
+  avatarNameProps?: AvatarNameProps;
+}
+
+function Avatar({ avatarRootProps, name, avatarImageProps, avatarFallbackProps, avatarNameProps = { hideAvatarName: true } }: Props) {
+  const [imgError, setImgError] = useState(false);
+  const firstLetters = getFirstLetters(name);
+
+  /**
+   * Rerender olma durumunda rengin değişmemesi için memoize edildi.
+   */
+  const computedBackgroundColor = getRandomColor();
+
+  return (
+    <>
+      <AvatarPrimitive.Root
+        {...avatarRootProps}
+        className={cn('relative flex size-8 items-center justify-center overflow-hidden rounded-full', avatarRootProps?.className)}
+      >
+        {imgError ? (
+          <AvatarPrimitive.Fallback
+            {...avatarFallbackProps}
+            className={cn(
+              'flex size-full items-center justify-center text-sm leading-normal text-white',
+              computedBackgroundColor,
+              avatarFallbackProps?.className
+            )}
+          >
+            {firstLetters}
+          </AvatarPrimitive.Fallback>
+        ) : (
+          <img
+            alt={name || 'avatar-default-alt-text'}
+            draggable={false}
+            onError={() => {
+              setImgError(true);
+            }}
+            {...avatarImageProps}
+            src={avatarImageProps ? avatarImageProps.src || '/dummy.png' : '/dummy.png'}
+            className={cn('size-fit', avatarImageProps?.className)}
+          />
+        )}
+      </AvatarPrimitive.Root>
+      {avatarNameProps.hideAvatarName ? null : <span {...avatarNameProps.nameElementProps}>{name}</span>}
+    </>
+  );
+}
+export default Avatar;
--- app/layout/title-bar/user-actions.tsx
+++ app/layout/title-bar/user-actions.tsx
@@ -1,7 +1,33 @@
+import { Link } from '@tanstack/react-router';
+
 import Avatar from '@app/components/ui/avatar';
+import Button from '@app/components/ui/button';
 import Dropdown from '@app/components/ui/dropdown';
 import Icon from '@app/components/ui/icon';
+import type { IconListProps } from '@app/components/ui/icon/icon-list';
 import useCurrentUserQuery from '@app/services/use-current-user-query';
+
+interface DropdownItem {
+  dropdownItemId: string;
+  label: string;
+  icon: IconListProps;
+  route: string;
+}
+
+const dropdownItems: DropdownItem[] = [
+  {
+    dropdownItemId: 'profile',
+    label: 'Profilim',
+    icon: 'user',
+    route: '/profile',
+  },
+  {
+    dropdownItemId: 'logout',
+    label: 'Çıkış Yap',
+    icon: 'logout',
+    route: '/logout',
+  },
+];
 
 function UserActions() {
   const { data, isLoading } = useCurrentUserQuery();
@@ -13,48 +39,39 @@ function UserActions() {
   if (!data) return null;
 
   return (
-    <Dropdown
-      triggerProps={{
-        className: 'px-1',
-        children: (
-          <>
-            <Avatar
-              name={data.name}
-              avatarRootProps={{
-                className: 'size-6 rounded-sm',
-              }}
-            />
-            <Icon
-              name="chevron-down"
-              className="size-4"
-            />
-          </>
-        ),
-      }}
-      dropdownItems={[
-        {
-          itemType: 'default',
-          dropdownItemId: 'default-1',
-          itemProps: {
-            children: 'Profilim',
-            icon: {
-              name: 'user',
-            },
-          },
-        },
-        {
-          itemType: 'link',
-          dropdownItemId: 'link-1',
-          itemProps: {
-            to: '/logout',
-            children: 'Çıkış Yap',
-            icon: {
-              name: 'logout',
-            },
-          },
-        },
-      ]}
-    />
+    <Dropdown>
+      <Dropdown.Trigger asChild>
+        <Button
+          size="sm"
+          variant="secondary"
+          className="px-0.5!"
+        >
+          <Avatar
+            name={data.name}
+            avatarRootProps={{
+              className: 'size-6 rounded-sm',
+            }}
+          />
+          <Icon
+            name="chevron-down"
+            className="size-4"
+          />
+        </Button>
+      </Dropdown.Trigger>
+      <Dropdown.Content>
+        {dropdownItems.map(item => (
+          <Dropdown.Item
+            key={item.dropdownItemId}
+            asChild
+          >
+            <Link to={item.route}>
+              <Icon name={item.icon} />
+              {item.label}
+            </Link>
+          </Dropdown.Item>
+        ))}
+      </Dropdown.Content>
+    </Dropdown>
   );
 }
 
