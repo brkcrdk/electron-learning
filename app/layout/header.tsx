72c09732e59cc9ba55b32c66c7fb17c067d272e9
Burak <burak@90pixel.com>
2025-12-09 21:26:29 +0300

    refactor: update Header component to integrate Sidebar and Link
    - Replaced commented-out navigation elements with a functional Sidebar structure.
    - Integrated Link component for navigation, enhancing user interaction.
    - Streamlined the Sidebar.MenuButton to improve layout and accessibility.

--- app/layout/header.tsx
+++ app/layout/header.tsx
@@ -1,47 +1,29 @@
-// import Icon from '@app/components/ui/icon';
+import { Link } from '@tanstack/react-router';
 
+import Icon from '@app/components/ui/icon';
 import Sidebar from '@app/components/ui/sidebar';
 
 function Header() {
   return (
-    // <nav className="navbar bg-base-300 w-full">
-    //   <label
-    //     htmlFor="sidebar-drawer"
-    //     aria-label="open sidebar"
-    //     className="btn btn-square btn-ghost"
-    //   >
-    //     <Icon name="sidebar-expand" />
-    //   </label>
-    //   <div className="px-4">Navbar Title</div>
-    // </nav>
-    // <SidebarHeader>
-    //   <SidebarMenu>
-    //     <SidebarMenuItem>
-    //       <SidebarMenuButton
-    //         size="lg"
-    //         asChild
-    //       >
-    //         <a href="#">
-    //           <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-    //             <Command className="size-4" />
-    //           </div>
-    //           <div className="grid flex-1 text-left text-sm leading-tight">
-    //             <span className="truncate font-medium">Acme Inc</span>
-    //             <span className="truncate text-xs">Enterprise</span>
-    //           </div>
-    //         </a>
-    //       </SidebarMenuButton>
-    //     </SidebarMenuItem>
-    //   </SidebarMenu>
-    // </SidebarHeader>
     <Sidebar.Header>
       <Sidebar.Menu>
         <Sidebar.MenuItem>
           <Sidebar.MenuButton
             size="lg"
-            // asChild
+            asChild
           >
-            asdad
+            <Link to="/">
+              <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                <Icon
+                  name="home"
+                  className="size-4"
+                />
+              </div>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-medium">Acme Inc</span>
+                <span className="truncate text-xs">Enterprise</span>
+              </div>
+            </Link>
           </Sidebar.MenuButton>
         </Sidebar.MenuItem>
       </Sidebar.Menu>
