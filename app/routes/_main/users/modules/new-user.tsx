21a833170200c5d7f30e66c99e02caccaca0d00d
Burak <burak@90pixel.com>
2025-12-10 12:46:09 +0300

    refactor: update NewUser and UserForm components for improved structure and usability
    - Refactored NewUser component to utilize a more streamlined Drawer structure, enhancing user interaction for adding new users.
    - Commented out unused imports and code in UserForm to improve clarity and maintainability.
    - Simplified the form layout and integrated Button component for user actions, improving overall user experience.

--- app/routes/_main/users/modules/new-user.tsx
+++ app/routes/_main/users/modules/new-user.tsx
@@ -1,50 +1,65 @@
-import { Dialog } from 'radix-ui';
-import { FormProvider, useForm } from 'react-hook-form';
+// import { Dialog } from 'radix-ui';
+// import { FormProvider, useForm } from 'react-hook-form';
 
+import Button from '@app/components/ui/button';
 import Drawer from '@app/components/ui/drawer';
 
-import type { UserFormInputs } from './user-form';
-import UserForm from './user-form';
+// import Drawer from '@app/components/ui/drawer';
+
+// import type { UserFormInputs } from './user-form';
+// import UserForm from './user-form';
 
 function NewUser() {
-  const form = useForm<UserFormInputs>({
-    defaultValues: {
-      name: '',
-      email: '',
-      password: '',
-      roles: 'user',
-      isActive: true,
-    },
-  });
+  // const form = useForm<UserFormInputs>({
+  //   defaultValues: {
+  //     name: '',
+  //     email: '',
+  //     password: '',
+  //     roles: 'user',
+  //     isActive: true,
+  //   },
+  // });
 
   return (
-    <Drawer
-      triggerProps={{ children: 'Kullanıcı Ekle' }}
-      rootProps={{
-        onOpenChange: open => {
-          if (!open) {
-            form.reset();
-          }
-        },
-      }}
-    >
-      <Drawer.Header
-        title="Yeni Kullanıcı Oluştur"
-        description="Yeni bir kullanıcı oluşturmak için lütfen bilgilerinizi giriniz."
-      />
-      <FormProvider {...form}>
-        <UserForm onSubmit={values => console.log('values', values)} />
-      </FormProvider>
-      <Drawer.Footer>
-        <Dialog.Close className="btn btn-soft flex-1">Kapat</Dialog.Close>
-        <button
-          form="user-form-inputs"
-          type="submit"
-          className="btn btn-primary btn-soft flex-1"
-        >
-          Kaydet
-        </button>
-      </Drawer.Footer>
+    // <Drawer
+    //   triggerProps={{ children: 'Kullanıcı Ekle' }}
+    //   rootProps={{
+    //     onOpenChange: open => {
+    //       if (!open) {
+    //         form.reset();
+    //       }
+    //     },
+    //   }}
+    // >
+    //   <Drawer.Header
+    //     title="Yeni Kullanıcı Oluştur"
+    //     description="Yeni bir kullanıcı oluşturmak için lütfen bilgilerinizi giriniz."
+    //   />
+    //   <FormProvider {...form}>
+    //     <UserForm onSubmit={values => console.log('values', values)} />
+    //   </FormProvider>
+    //   <Drawer.Footer>
+    //     <Dialog.Close className="btn btn-soft flex-1">Kapat</Dialog.Close>
+    //     <button
+    //       form="user-form-inputs"
+    //       type="submit"
+    //       className="btn btn-primary btn-soft flex-1"
+    //     >
+    //       Kaydet
+    //     </button>
+    //   </Drawer.Footer>
+    // </Drawer>
+    <Drawer>
+      <Drawer.Trigger>Kullanıcı Ekle</Drawer.Trigger>
+      <Drawer.Content>
+        <Drawer.Header>
+          <Drawer.Title>Yeni Kullanıcı Oluştur</Drawer.Title>
+          <Drawer.Description>Yeni bir kullanıcı oluşturmak için lütfen bilgilerinizi giriniz.</Drawer.Description>
+        </Drawer.Header>
+        <Drawer.Footer>
+          <Button>Kullanıcıyı Ekle</Button>
+        </Drawer.Footer>
+      </Drawer.Content>
     </Drawer>
   );
 }
--- app/routes/_main/users/modules/user-form.tsx
+++ app/routes/_main/users/modules/user-form.tsx
@@ -1,91 +1,91 @@
-import { Controller, useFormContext } from 'react-hook-form';
+// import { Controller, useFormContext } from 'react-hook-form';
 
-import Input from '@app/components/ui/input';
-import PasswordInput from '@app/components/ui/password-input';
-import Select from '@app/components/ui/select';
-import Switch from '@app/components/ui/switch';
-import { emailValidation, passwordValidation } from '@app/utils/form-validations';
-import type { User } from '@db/schema';
+// import Input from '@app/components/ui/input';
+// import PasswordInput from '@app/components/ui/password-input';
+// import Select from '@app/components/ui/select';
+// import Switch from '@app/components/ui/switch';
+// import { emailValidation, passwordValidation } from '@app/utils/form-validations';
+// import type { User } from '@db/schema';
 
-export interface UserFormInputs {
-  name: string;
-  email: string;
-  password: string;
-  roles: User['roles'];
-  isActive: boolean;
-}
+// export interface UserFormInputs {
+//   name: string;
+//   email: string;
+//   password: string;
+//   roles: User['roles'];
+//   isActive: boolean;
+// }
 
-interface Props {
-  onSubmit: (data: UserFormInputs) => void;
-}
+// interface Props {
+//   onSubmit: (data: UserFormInputs) => void;
+// }
 
-function UserForm({ onSubmit }: Props) {
-  const { control, handleSubmit } = useFormContext<UserFormInputs>();
+// function UserForm({ onSubmit }: Props) {
+//   const { control, handleSubmit } = useFormContext<UserFormInputs>();
 
-  return (
-    <form
-      className="mt-6 grid gap-3"
-      id="user-form-inputs"
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <Controller
-        control={control}
-        name="name"
-        rules={{ required: 'Adı Soyadı alanı zorunludur' }}
-        render={({ field, fieldState }) => (
-          <Input
-            label="Adı Soyadı"
-            placeholder="Adı Soyadı"
-            error={fieldState.error?.message}
-            {...field}
-          />
-        )}
-      />
-      <Controller
-        control={control}
-        name="email"
-        rules={emailValidation}
-        render={({ field, fieldState }) => (
-          <Input
-            label="E-posta"
-            placeholder="test@example.com"
-            error={fieldState.error?.message}
-            {...field}
-          />
-        )}
-      />
-      <Controller
-        control={control}
-        name="password"
-        rules={passwordValidation}
-        render={({ field, fieldState }) => (
-          <PasswordInput
-            label="Şifre"
-            error={fieldState.error?.message}
-            inputProps={{
-              placeholder: '********',
-              id: 'password',
-              value: field.value,
-              onChange: field.onChange,
-            }}
-          />
-        )}
-      />
+//   return (
+//     <form
+//       className="mt-6 grid gap-3"
+//       id="user-form-inputs"
+//       onSubmit={handleSubmit(onSubmit)}
+//     >
+//       <Controller
+//         control={control}
+//         name="name"
+//         rules={{ required: 'Adı Soyadı alanı zorunludur' }}
+//         render={({ field, fieldState }) => (
+//           <Input
+//             label="Adı Soyadı"
+//             placeholder="Adı Soyadı"
+//             error={fieldState.error?.message}
+//             {...field}
+//           />
+//         )}
+//       />
+//       <Controller
+//         control={control}
+//         name="email"
+//         rules={emailValidation}
+//         render={({ field, fieldState }) => (
+//           <Input
+//             label="E-posta"
+//             placeholder="test@example.com"
+//             error={fieldState.error?.message}
+//             {...field}
+//           />
+//         )}
+//       />
+//       <Controller
+//         control={control}
+//         name="password"
+//         rules={passwordValidation}
+//         render={({ field, fieldState }) => (
+//           <PasswordInput
+//             label="Şifre"
+//             error={fieldState.error?.message}
+//             inputProps={{
+//               placeholder: '********',
+//               id: 'password',
+//               value: field.value,
+//               onChange: field.onChange,
+//             }}
+//           />
+//         )}
+//       />
 
-      <Select
-        label="Kullanıcı Rolü"
-        options={[
-          { label: 'Admin', value: 'admin' },
-          { label: 'Kullanıcı', value: 'user' },
-        ]}
-      />
-      <Switch
-        label="Aktiflik Durumu"
-        id="status"
-        defaultChecked
-      />
-    </form>
-  );
-}
+//       <Select
+//         label="Kullanıcı Rolü"
+//         options={[
+//           { label: 'Admin', value: 'admin' },
+//           { label: 'Kullanıcı', value: 'user' },
+//         ]}
+//       />
+//       <Switch
+//         label="Aktiflik Durumu"
+//         id="status"
+//         defaultChecked
+//       />
+//     </form>
+//   );
+// }
 
-export default UserForm;
+// export default UserForm;
