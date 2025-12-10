8bbccc3e16a7c74c978b9754fd59ca9d18514deb
Burak <burak@90pixel.com>
2025-12-10 12:42:33 +0300

    refactor: simplify TableHeader component layout for improved readability
    - Streamlined the rendering logic in the TableHeader component by removing unnecessary conditional checks.
    - Ensured consistent layout for search and action elements, enhancing the overall structure and maintainability of the code.

--- app/components/data-table/table-header/index.tsx
+++ app/components/data-table/table-header/index.tsx
@@ -11,18 +11,18 @@ function TableHeader({ tableTitle, searchProps, tableActions }: TableHeaderProps
   return (
     <header className="flex flex-col gap-4">
       {tableTitle && <h1 className="text-2xl font-medium">{tableTitle}</h1>}
-      {(searchProps || tableActions) && (
-        <div className="flex items-center justify-between gap-2">
-          {searchProps && (
-            <TableSearchBar
-              value={searchProps.value}
-              placeholder={searchProps.placeholder}
-              onSearch={searchProps.onSearch}
-            />
-          )}
-          {tableActions && tableActions.length > 0 ? <TableActions actions={tableActions} /> : null}
-        </div>
-      )}
+      <div className="flex items-center justify-between">
+        {searchProps ? (
+          <TableSearchBar
+            value={searchProps.value}
+            placeholder={searchProps.placeholder}
+            onSearch={searchProps.onSearch}
+          />
+        ) : (
+          <span />
+        )}
+        {tableActions ? <TableActions actions={tableActions} /> : null}
+      </div>
     </header>
   );
 }
