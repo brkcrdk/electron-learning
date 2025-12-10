890f3d1f6011741f6ddb0abfa987766f90a98ec9
Burak <burak@90pixel.com>
2025-12-10 12:43:09 +0300

    refactor: replace Dropdown with Drawer in EditUser component for improved UI interaction
    - Simplified ColumnActions component to directly render EditUser.
    - Updated EditUser component to utilize Drawer for user editing functionality, enhancing the user experience with a more interactive design.

--- app/routes/_main/users/hooks/use-columns/column-actions/index.tsx
+++ app/routes/_main/users/hooks/use-columns/column-actions/index.tsx
@@ -1,24 +1,7 @@
-import Button from '@app/components/ui/button';
-import Dropdown from '@app/components/ui/dropdown';
-import Icon from '@app/components/ui/icon';
-
 import EditUser from '../../../modules/edit-user';
+
 function ColumnActions() {
-  return (
-    <Dropdown>
-      <Dropdown.Trigger asChild>
-        <Button
-          variant="ghost"
-          size="icon"
-        >
-          <Icon name="more-horizontal" />
-        </Button>
-      </Dropdown.Trigger>
-      <Dropdown.Content>
-        <EditUser />
-      </Dropdown.Content>
-    </Dropdown>
-  );
+  return <EditUser />;
 }
 
 export default ColumnActions;
--- app/routes/_main/users/modules/edit-user.tsx
+++ app/routes/_main/users/modules/edit-user.tsx
@@ -1,26 +1,21 @@
-import Dropdown from '@app/components/ui/dropdown';
+import Drawer from '@app/components/ui/drawer';
 import Icon from '@app/components/ui/icon';
 
 function EditUser() {
   return (
-    // <Drawer
-    //   drawerId="edit-user-drawer"
-    //   triggerProps={{
-    //     children: (
-    //       <Icon
-    //         name="pencil"
-    //         className="size-4"
-    //       />
-    //     ),
-    //     className: 'btn btn-square btn-sm',
-    //   }}
-    // >
-    //   <Drawer.Header title="Kullanıcı Düzenle" />
-    // </Drawer>
-    <Dropdown.Item>
-      <Icon name="pencil" />
-      Düzenle
-    </Dropdown.Item>
+    <Drawer>
+      <Drawer.Trigger size="icon-sm">
+        <Icon
+          name="pencil"
+          className="size-4"
+        />
+      </Drawer.Trigger>
+      <Drawer.Content>
+        <Drawer.Header>
+          <Drawer.Title>Kullanıcı Düzenle</Drawer.Title>
+        </Drawer.Header>
+      </Drawer.Content>
+    </Drawer>
   );
 }
 
