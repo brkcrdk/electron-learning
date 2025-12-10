86f5cf1dd5e047f2171e82a279da85d40803355f
Burak <burak@90pixel.com>
2025-12-10 11:55:50 +0300

    refactor: enable useColumns hook in users route for data table integration
    - Uncommented the useColumns hook to utilize dynamic column definitions in the DataTable component.
    - Updated the columns prop in DataTable to use the columns from useColumns instead of an empty array, enhancing the table's functionality.

--- app/routes/_main/users/index.tsx
+++ app/routes/_main/users/index.tsx
@@ -4,7 +4,7 @@ import { createFileRoute } from '@tanstack/react-router';
 import DataTable from '@app/components/data-table';
 import useUserListQuery from '@app/services/use-user-list-query';
 
-// import useColumns from './hooks/use-columns';
+import useColumns from './hooks/use-columns';
 // import useTableActions from './hooks/use-table-actions';
 
 export const Route = createFileRoute('/_main/users/')({
@@ -14,14 +14,13 @@ export const Route = createFileRoute('/_main/users/')({
 function RouteComponent() {
   const { data, isLoading } = useUserListQuery();
 
-  // const columns = useColumns();
+  const columns = useColumns();
   // const tableActions = useTableActions();
 
   return (
     <DataTable
       tableTitle="Kullanıcı Listesi"
-      // columns={columns}
-      columns={[]}
+      columns={columns}
       data={data ? data : []}
       isLoading={isLoading}
       // tableActions={tableActions}
