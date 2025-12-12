bdada5b2b3247f865eb94c6cfb5bc5d1a2baca4e
Burak <burak@90pixel.com>
2025-12-12 16:15:13 +0300

    fix: update app initialization method in main.ts
    - Changed the app initialization from 'app.on("ready")' to 'app.whenReady().then()' for improved handling of asynchronous operations during application startup.

--- src/main.ts
+++ src/main.ts
@@ -52,7 +52,7 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', async () => {
+app.whenReady().then(async () => {
   /**
    * Uygulama production ortamında ise veritabanını başlat
    */
