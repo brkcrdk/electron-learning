9c04301b561ec0d7050840ad3122ba60f72074fd
Burak <burak@90pixel.com>
2025-12-14 18:01:33 +0300

    refactor: consolidate API handlers into modular structure
    - Replaced individual user and category event handlers with a unified `userApi` and `categoryApi` for improved organization.
    - Introduced `sessionApi` to manage session-related functionalities, enhancing the modularity of the API structure.
    - Updated the `registerApiHandlers` function to reflect the new modular approach, streamlining the API registration process.

--- api/index.ts
+++ api/index.ts
@@ -1,39 +1,14 @@
-import createCategoryHandler from './create-category';
-import createUserHandler from './create-user';
-import deleteCategoryHandler from './delete-category';
-import deleteUserHandler from './delete-user';
-import getCategoryListHandler from './get-category-list';
-import getCurrentUserHandler from './get-current-user';
-import getUserListHandler from './get-user-list';
-import loginHandler from './login';
-import logoutHandler from './logout';
+import categoryApi from './category-api';
+import sessionApi from './session-api';
 import superAdminApi from './super-admin-api';
-import updateCategoryHandler from './update-category';
-import updateUserHandler from './update-user';
 import uploadFileApi from './upload-file-api';
+import userApi from './user-api';
 
 function registerApiHandlers() {
   superAdminApi();
-
-  // User Events
-  createUserHandler();
-  getUserListHandler();
-  updateUserHandler();
-  deleteUserHandler();
-
-  getCurrentUserHandler();
-
-  // Auth Events
-  loginHandler();
-  logoutHandler();
-
-  // Category Events
-  getCategoryListHandler();
-  createCategoryHandler();
-  updateCategoryHandler();
-  deleteCategoryHandler();
-
-  // File Upload Events
+  userApi();
+  categoryApi();
+  sessionApi();
   uploadFileApi();
 }
 
