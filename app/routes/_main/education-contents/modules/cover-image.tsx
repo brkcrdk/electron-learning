1a0b1f1e5d1b846c3ab419127ba040abf0a5ec42
Burak <burak@90pixel.com>
2025-12-15 00:28:18 +0300

    feat: add CoverImage component for file upload functionality
    - Introduced a new CoverImage component that integrates with react-hook-form for managing cover image uploads.
    - Utilized the useFileUpload hook to handle image uploads, including error handling and progress tracking.
    - Implemented validation to ensure the cover image field is required, enhancing form integrity.

---
+++ app/routes/_main/education-contents/modules/cover-image.tsx
@@ -0,0 +1,41 @@
+import { Controller, useFormContext } from 'react-hook-form';
+
+import FileUploadField from '@app/components/form-fields/file-upload-field';
+import useFileUpload from '@app/hooks/use-file-upload';
+
+import type { MaterialFormInputs } from './material-form';
+
+function CoverImage() {
+  const { control, setValue } = useFormContext<MaterialFormInputs>();
+
+  const { handleUpload, uploadState, resetUploadState } = useFileUpload({
+    uploadType: 'images',
+    onComplete: completed => {
+      setValue('cover_image', completed.response);
+    },
+  });
+
+  return (
+    <Controller
+      control={control}
+      name="cover_image"
+      rules={{ required: 'Kapak resmi alanı zorunludur' }}
+      render={({ fieldState }) => (
+        <FileUploadField
+          label="Kapak Resmi:"
+          inputId="cover_image"
+          error={fieldState.error?.message}
+          uploadProviderProps={{
+            onChange: handleUpload,
+            accept: 'image/*',
+            sizeLimit: 5,
+          }}
+          uploadingProgress={uploadState}
+          onReset={resetUploadState}
+        />
+      )}
+    />
+  );
+}
+
+export default CoverImage;
