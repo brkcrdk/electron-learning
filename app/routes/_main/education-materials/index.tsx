2c4b69eb39622bbb298f49d93b5619db6f915f33
Burak <burak@90pixel.com>
2025-12-14 18:08:46 +0300

    refactor: replace file upload UI with data table for education materials
    - Removed the file upload interface and integrated a DataTable component to display education materials.
    - Updated the RouteComponent to utilize the DataTable, enhancing the user experience by providing a structured view of materials.
    - Simplified the component by eliminating unnecessary upload logic and state management.

--- app/routes/_main/education-materials/index.tsx
+++ app/routes/_main/education-materials/index.tsx
@@ -1,29 +1,23 @@
 import { createFileRoute } from '@tanstack/react-router';
 
-import Button from '@app/components/ui/button';
-import UploadProvider from '@app/components/ui/upload-provider';
-import useFileUpload from '@app/hooks/use-file-upload';
+import DataTable from '@app/components/data-table';
 
 export const Route = createFileRoute('/_main/education-materials/')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const { handleUpload, uploadState, handleCancel, resetUploadState } = useFileUpload({ uploadType: 'pdfs' });
-
   return (
-    <div>
-      <UploadProvider
-        accept="application/pdf"
-        multiple={false}
-        onChange={async file => {
-          handleUpload(file);
-        }}
-      >
-        <Button>Upload Education Materials</Button>
-      </UploadProvider>
-      <pre>{JSON.stringify(uploadState, null, 4)}</pre>
-      <Button onClick={handleCancel}>Cancel</Button>
-    </div>
+    <DataTable
+      tableTitle="Eğitim Malzemeleri Listesi"
+      columns={[]}
+      data={[]}
+      isLoading={false}
+      tableActions={[]}
+      // columns={columns}
+      // data={data ? data : []}
+      // isLoading={isLoading}
+      // tableActions={tableActions}
+    />
   );
 }
