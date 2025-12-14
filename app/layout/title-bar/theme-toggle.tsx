b667dc06f85e00fccf2f66835a3bfb3bcb474e33
Burak <burak@90pixel.com>
2025-12-14 14:15:31 +0300

    feat: enable theme toggle functionality in the title bar
    - Reintroduced the ThemeToggle component in the title bar for user accessibility.
    - Implemented theme state management and effect hooks to retrieve and set the current theme.
    - Enabled the toggleTheme function to switch between light and dark themes based on user interaction.

--- app/layout/title-bar/theme-toggle.tsx
+++ app/layout/title-bar/theme-toggle.tsx
@@ -6,37 +6,37 @@ import Icon from '@app/components/ui/icon';
 import type { ThemeType } from '../../../store/theme';
 
 function ThemeToggle() {
-  // const [theme, setTheme] = useState<ThemeType>('light');
+  const [theme, setTheme] = useState<ThemeType>('light');
 
-  // useEffect(() => {
-  //   const getTheme = async () => {
-  //     const theme = await window.store.getTheme();
-  //     setTheme(theme.id);
-  //   };
+  useEffect(() => {
+    const getTheme = async () => {
+      const theme = await window.store.getTheme();
+      setTheme(theme.id);
+    };
 
-  //   getTheme();
-  // }, []);
+    getTheme();
+  }, []);
 
-  // const toggleTheme = async () => {
-  //   if (theme === 'light') {
-  //     await window.store.setTheme('dark');
-  //     document.documentElement.classList.add('dark');
-  //     document.documentElement.style.colorScheme = 'dark';
-  //     document.documentElement.classList.remove('light');
-  //     setTheme('dark');
-  //   } else {
-  //     await window.store.setTheme('light');
-  //     document.documentElement.classList.remove('dark');
-  //     document.documentElement.classList.add('light');
-  //     document.documentElement.style.colorScheme = 'light';
-  //     setTheme('light');
-  //   }
-  // };
+  const toggleTheme = async () => {
+    if (theme === 'light') {
+      await window.store.setTheme('dark');
+      document.documentElement.classList.add('dark');
+      document.documentElement.style.colorScheme = 'dark';
+      document.documentElement.classList.remove('light');
+      setTheme('dark');
+    } else {
+      await window.store.setTheme('light');
+      document.documentElement.classList.remove('dark');
+      document.documentElement.classList.add('light');
+      document.documentElement.style.colorScheme = 'light';
+      setTheme('light');
+    }
+  };
   return (
     <Button
       variant="ghost"
       size="icon"
-      // onClick={toggleTheme}
+      onClick={toggleTheme}
     >
       <Icon name="contrast-filled" />
     </Button>
