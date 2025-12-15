5ec0ce0a3a8aacece28b01b4ce57cf9557cc7705
Burak <burak@90pixel.com>
2025-12-16 02:12:14 +0300

    feat: enhance EducationCard component with content type icons and tooltips
    - Added content type labels and icons for different media types in the EducationCard component.
    - Integrated Tooltip component to display content type information on hover.
    - Updated button text for improved clarity in the EducationCard layout.

--- app/routes/_main/my-educations/modules/education-card/index.tsx
+++ app/routes/_main/my-educations/modules/education-card/index.tsx
@@ -1,9 +1,36 @@
 import Badge from '@app/components/ui/badge';
 import Button from '@app/components/ui/button';
 import Card from '@app/components/ui/card';
+import Icon from '@app/components/ui/icon';
+import type { IconListProps } from '@app/components/ui/icon/icon-list';
 import ImageFallback from '@app/components/ui/image-fallback';
+import Tooltip from '@app/components/ui/tooltip';
 import getContentPath from '@app/utils/get-content-path';
-import type { EducationListItem } from '@db/schema';
+import type { EducationListItem, MediaFileTypes } from '@db/schema';
+
+interface ContentTypeOption {
+  label: string;
+  icon: IconListProps;
+}
+
+const contentTypeLabels: Record<MediaFileTypes, ContentTypeOption> = {
+  pdfs: {
+    label: 'PDF İçeriği',
+    icon: 'file-pdf',
+  },
+  video: {
+    label: 'Video İçeriği',
+    icon: 'file-video',
+  },
+  stories: {
+    label: 'Articulate İçeriği',
+    icon: 'file-easel',
+  },
+  images: {
+    label: 'Resim İçeriği',
+    icon: 'file-image',
+  },
+};
 
 interface Props {
   education: EducationListItem;
@@ -20,9 +47,22 @@ function EducationCard({ education }: Props) {
         <Badge className="absolute right-4 top-4">{education.category.name}</Badge>
       </Card.Header>
       <Card.Content className="grid gap-4 p-4">
-        <Card.Title>{education.name}</Card.Title>
+        <div className="flex items-center gap-2">
+          <Card.Title>{education.name}</Card.Title>
+          <Tooltip>
+            <Tooltip.Trigger>
+              <Icon
+                name={contentTypeLabels[education.educationMaterial.contentType].icon}
+                className="size-4"
+              />
+            </Tooltip.Trigger>
+            <Tooltip.Content>
+              <p>{contentTypeLabels[education.educationMaterial.contentType].label}</p>
+            </Tooltip.Content>
+          </Tooltip>
+        </div>
         <Card.Description>{education.description}</Card.Description>
-        <Button>İçeriği Görüntüle</Button>
+        <Button>Eğitimi Görüntüle</Button>
       </Card.Content>
     </Card>
   );
