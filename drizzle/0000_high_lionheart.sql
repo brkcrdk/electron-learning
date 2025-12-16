30a299e9ea9239959edd1e2def6c07629901c686
Burak <burak@90pixel.com>
2025-12-16 10:02:39 +0300

    feat: add initial database schema for education platform
    - Introduced new SQL schema for categories, education materials, educations, users, and media files.
    - Created indexes and foreign key relationships to ensure data integrity and optimize queries.
    - Added journal and snapshot metadata files for versioning and tracking changes in the database structure.

---
+++ drizzle/0000_high_lionheart.sql
@@ -0,0 +1,92 @@
+CREATE TABLE `categories` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text NOT NULL,
+	`slug` text NOT NULL,
+	`description` text NOT NULL,
+	`parent_id` integer,
+	`created_by` integer NOT NULL,
+	`created_at` integer DEFAULT (unixepoch()) NOT NULL,
+	`updated_at` integer DEFAULT (unixepoch()) NOT NULL,
+	FOREIGN KEY (`parent_id`) REFERENCES `categories`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`created_by`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `categories_slug_unique` ON `categories` (`slug`);--> statement-breakpoint
+CREATE INDEX `idx_category_parent_id` ON `categories` (`parent_id`);--> statement-breakpoint
+CREATE TABLE `education_assignees` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`education_id` integer NOT NULL,
+	`assignee_user_id` integer NOT NULL,
+	`created_at` integer DEFAULT (unixepoch()) NOT NULL,
+	`updated_at` integer DEFAULT (unixepoch()) NOT NULL,
+	FOREIGN KEY (`education_id`) REFERENCES `educations`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`assignee_user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE INDEX `idx_education_assignees_education_id` ON `education_assignees` (`education_id`);--> statement-breakpoint
+CREATE INDEX `idx_education_assignees_assignee_user_id` ON `education_assignees` (`assignee_user_id`);--> statement-breakpoint
+CREATE UNIQUE INDEX `unique_education_user` ON `education_assignees` (`education_id`,`assignee_user_id`);--> statement-breakpoint
+CREATE TABLE `education_materials` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text NOT NULL,
+	`description` text NOT NULL,
+	`content_type` text NOT NULL,
+	`content_file_id` integer NOT NULL,
+	`created_by` integer NOT NULL,
+	`created_at` integer DEFAULT (unixepoch()) NOT NULL,
+	`updated_at` integer DEFAULT (unixepoch()) NOT NULL,
+	FOREIGN KEY (`content_file_id`) REFERENCES `media_files`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`created_by`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE INDEX `idx_educations_content_file_id` ON `education_materials` (`content_file_id`);--> statement-breakpoint
+CREATE INDEX `idx_education_materials_created_by` ON `education_materials` (`created_by`);--> statement-breakpoint
+CREATE INDEX `idx_educations_content_type` ON `education_materials` (`content_type`);--> statement-breakpoint
+CREATE TABLE `educations` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text NOT NULL,
+	`description` text NOT NULL,
+	`category_id` integer NOT NULL,
+	`cover_image_id` integer,
+	`education_materials` integer NOT NULL,
+	`created_by` integer NOT NULL,
+	`created_at` integer DEFAULT (unixepoch()) NOT NULL,
+	`updated_at` integer DEFAULT (unixepoch()) NOT NULL,
+	FOREIGN KEY (`category_id`) REFERENCES `categories`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`cover_image_id`) REFERENCES `media_files`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`education_materials`) REFERENCES `education_materials`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`created_by`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE INDEX `idx_educations_category_id` ON `educations` (`category_id`);--> statement-breakpoint
+CREATE INDEX `idx_educations_cover_image_id` ON `educations` (`cover_image_id`);--> statement-breakpoint
+CREATE INDEX `idx_educations_education_material` ON `educations` (`education_materials`);--> statement-breakpoint
+CREATE INDEX `idx_educations_created_by` ON `educations` (`created_by`);--> statement-breakpoint
+CREATE TABLE `users` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text NOT NULL,
+	`username` text NOT NULL,
+	`password` text NOT NULL,
+	`role` text DEFAULT 'user' NOT NULL,
+	`status` text DEFAULT 'active' NOT NULL,
+	`created_at` integer DEFAULT (unixepoch()) NOT NULL,
+	`updated_at` integer DEFAULT (unixepoch()) NOT NULL,
+	`last_login_at` integer
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `users_username_unique` ON `users` (`username`);--> statement-breakpoint
+CREATE INDEX `roles_idx` ON `users` (`role`);--> statement-breakpoint
+CREATE TABLE `media_files` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`file_path` text NOT NULL,
+	`file_name` text NOT NULL,
+	`file_size` integer NOT NULL,
+	`media_type` text NOT NULL,
+	`uploaded_by` integer,
+	`created_at` integer DEFAULT (unixepoch()) NOT NULL,
+	`updated_at` integer DEFAULT (unixepoch()) NOT NULL,
+	FOREIGN KEY (`uploaded_by`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE INDEX `idx_files_uploaded_by` ON `media_files` (`uploaded_by`);--> statement-breakpoint
+CREATE INDEX `idx_files_media_type` ON `media_files` (`media_type`);
