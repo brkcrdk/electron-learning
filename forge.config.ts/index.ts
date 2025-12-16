c6106b6b06cedffd72b1941a2accab5871195239
Burak <burak@90pixel.com>
2025-12-16 14:17:19 +0300

    feat: add DMG maker support to Electron Forge configuration
    - Integrated the MakerDMG into the Electron Forge configuration to enable DMG packaging for macOS.
    - Updated the makers array in the forge.config.ts to include the new MakerDMG instance, enhancing the build process for macOS applications.
    - Ensured compatibility with existing makers for a seamless build experience across platforms.

--- forge.config.ts/index.ts
+++ forge.config.ts/index.ts
@@ -1,5 +1,6 @@
 import { FuseV1Options, FuseVersion } from '@electron/fuses';
 import { MakerDeb } from '@electron-forge/maker-deb';
+import { MakerDMG } from '@electron-forge/maker-dmg';
 import { MakerRpm } from '@electron-forge/maker-rpm';
 import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerZIP } from '@electron-forge/maker-zip';
@@ -26,7 +27,7 @@ const config: ForgeConfig = {
       await copyDrizzleMigrations(buildPath);
     },
   },
-  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
+  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({}), new MakerDMG({})],
   plugins: [
     new AutoUnpackNativesPlugin({}),
     new VitePlugin({
