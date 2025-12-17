d0e472a858e0c7dcbd5c2c09f5388a725638b6ec
Burak <burak@90pixel.com>
2025-12-17 12:57:46 +0300

    feat: enhance education assignment columns with additional details
    - Added new columns for 'Eğitim', 'Atama Yapan', 'Atanan Kullanıcı Sayısı', and 'Oluşturulma Tarihi' to the education assignment list.
    - Integrated components for displaying education names, user details, and relative dates, improving the clarity and usability of the assignment list.
    - Updated the useColumns hook to reflect these changes, ensuring a more informative and user-friendly table layout.

--- app/routes/_main/education-assigments/hooks/use-columns.tsx
+++ app/routes/_main/education-assigments/hooks/use-columns.tsx
@@ -1,5 +1,8 @@
 import type { ColumnDef } from '@tanstack/react-table';
 
+import RelativeDateCell from '@app/components/table-cells/relative-date-cell';
+import UserCell from '@app/components/table-cells/user-cell';
+import Badge from '@app/components/ui/badge';
 import type { EducationAssignmentListItem } from '@db/schema';
 
 function useColumns(): ColumnDef<EducationAssignmentListItem>[] {
@@ -9,8 +12,57 @@ function useColumns(): ColumnDef<EducationAssignmentListItem>[] {
       accessorKey: 'title',
     },
     {
-      header: 'Açıklamasıı',
-      accessorKey: 'description',
+      header: 'Eğitim',
+      accessorKey: 'education',
+      cell: info => info.getValue(),
+      accessorFn: ({ education }) => {
+        return <Badge>{education.name}</Badge>;
+      },
+      meta: {
+        centeredColumn: true,
+      },
+    },
+    {
+      header: 'Atama Yapan',
+      accessorKey: 'createdBy',
+      cell: info => info.getValue(),
+      accessorFn: ({ createdBy }) => {
+        return <UserCell user={createdBy} />;
+      },
+      meta: {
+        centeredColumn: true,
+      },
+    },
+    {
+      header: 'Atanan Kullanıcı Sayısı',
+      accessorKey: 'updatedAt',
+      cell: info => info.getValue(),
+      accessorFn: ({ assignees }) => {
+        return assignees.length;
+      },
+      meta: {
+        centeredColumn: true,
+      },
+    },
+    {
+      header: 'Oluşturulma Tarihi',
+      accessorKey: 'createdAt',
+      cell: info => info.getValue(),
+      accessorFn: ({ createdAt }) => {
+        return <RelativeDateCell date={createdAt} />;
+      },
+      meta: {
+        centeredColumn: true,
+      },
+    },
+    {
+      header: 'İşlemler',
+      accessorKey: 'actions',
+      cell: info => info.getValue(),
+      accessorFn: ({ id }) => {
+        // return <EducationAssignmentActions assignment={id} />;
+        return 'işlemler';
+      },
     },
   ];
 }
