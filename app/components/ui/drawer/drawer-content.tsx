a4d4c84b9a25d845b0df187a3e8f6e385345cbf0
Burak <burak@90pixel.com>
2025-12-19 21:09:10 +0300

    refactor: enhance dialog and drawer components with improved styling
    - Added 'webkit-no-draggable' class to DialogContent and DrawerOverlay for better drag handling.
    - Updated DrawerContent styles to improve layout and responsiveness, ensuring a more consistent user experience.

--- app/components/ui/drawer/drawer-content.tsx
+++ app/components/ui/drawer/drawer-content.tsx
@@ -10,14 +10,14 @@ import DrawerPortal from './drawer-portal';
 function DrawerContent({ className, children, ...props }: ComponentProps<typeof Drawer.Content>) {
   return (
     <DrawerPortal data-slot="drawer-portal">
-      <DrawerOverlay />
+      <DrawerOverlay className="webkit-no-draggable" />
       <Drawer.Content
         data-slot="drawer-content"
         className={cn(
-          'group/drawer-content bg-background webkit-no-draggable fixed z-50 flex h-auto flex-col overflow-y-auto overflow-x-hidden p-4',
+          'group/drawer-content bg-background fixed z-50 flex h-auto max-h-[calc(100vh-1rem)] flex-col overflow-y-auto overflow-x-hidden rounded-lg p-4',
           'data-[vaul-drawer-direction=top]:inset-x-0 data-[vaul-drawer-direction=top]:top-0 data-[vaul-drawer-direction=top]:mb-24 data-[vaul-drawer-direction=top]:max-h-[80vh] data-[vaul-drawer-direction=top]:rounded-b-lg data-[vaul-drawer-direction=top]:border-b',
           'data-[vaul-drawer-direction=bottom]:inset-x-0 data-[vaul-drawer-direction=bottom]:bottom-0 data-[vaul-drawer-direction=bottom]:mt-24 data-[vaul-drawer-direction=bottom]:max-h-[80vh] data-[vaul-drawer-direction=bottom]:rounded-t-lg data-[vaul-drawer-direction=bottom]:border-t',
-          'data-[vaul-drawer-direction=right]:inset-y-0 data-[vaul-drawer-direction=right]:right-0 data-[vaul-drawer-direction=right]:w-3/4 data-[vaul-drawer-direction=right]:border-l data-[vaul-drawer-direction=right]:sm:max-w-md',
+          'data-[vaul-drawer-direction=right]:inset-y-2 data-[vaul-drawer-direction=right]:right-2 data-[vaul-drawer-direction=right]:w-3/4 data-[vaul-drawer-direction=right]:border data-[vaul-drawer-direction=right]:sm:max-w-md',
           'data-[vaul-drawer-direction=left]:inset-y-0 data-[vaul-drawer-direction=left]:left-0 data-[vaul-drawer-direction=left]:w-3/4 data-[vaul-drawer-direction=left]:border-r data-[vaul-drawer-direction=left]:sm:max-w-md',
           className
         )}
