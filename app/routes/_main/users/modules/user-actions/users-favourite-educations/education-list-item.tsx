14123f030c5fd9524f2e54e5a57ea4d7d12cc370
Burak <burak@90pixel.com>
2025-12-19 23:00:32 +0300

    feat: enhance EducationListItem and UsersFavouriteEducations components for improved display and pagination
    - Refactored EducationListItem to display detailed education information, including cover image, name, description, and categories.
    - Updated UsersFavouriteEducations to utilize useInfiniteQuery for paginated fetching of favorite educations, improving data handling and user experience.
    - Integrated loading and error states for better user feedback during data retrieval.

--- app/routes/_main/users/modules/user-actions/users-favourite-educations/education-list-item.tsx
+++ app/routes/_main/users/modules/user-actions/users-favourite-educations/education-list-item.tsx
@@ -1,5 +1,28 @@
-function EducationListItem() {
-  return <div>EducationListItem</div>;
+import Badge from '@app/components/ui/badge';
+import ImageFallback from '@app/components/ui/image-fallback';
+import getContentPath from '@app/utils/get-content-path';
+import type { UserEducationFavoriteListItem } from '@db/schema/user-education-favorites';
+
+interface Props {
+  education: UserEducationFavoriteListItem;
+}
+function EducationListItem({ education }: Props) {
+  return (
+    <li className="hover:bg-accent flex cursor-pointer items-center gap-3 p-2">
+      <ImageFallback
+        className="aspect-video size-20 rounded-sm object-cover"
+        src={getContentPath(education.education.coverImage?.filePath)}
+      />
+      <div className="flex flex-1 flex-col gap-1">
+        <h3>{education.education.name}</h3>
+        <span className="text-accent-foreground/50 text-sm">{education.education.description}</span>
+        <div className="flex items-center gap-2">
+          <Badge className="text-xs">{education.education.category.name}</Badge>
+          <Badge className="text-xs">{education.education.educationMaterial.name}</Badge>
+        </div>
+      </div>
+    </li>
+  );
 }
 
 export default EducationListItem;
--- app/routes/_main/users/modules/user-actions/users-favourite-educations/index.tsx
+++ app/routes/_main/users/modules/user-actions/users-favourite-educations/index.tsx
@@ -1,22 +1,72 @@
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 
+import Icon from '@app/components/ui/icon';
 import Tabs from '@app/components/ui/tabs';
 import getUsersFavouriteEducationsQuery from '@app/services/get-users-favourite-educations-query';
+import queryKeys from '@app/services/query-keys';
+
+import EducationListItem from './education-list-item';
 
 interface Props {
   userId: number;
 }
 function UsersFavouriteEducations({ userId }: Props) {
-  const { data, isLoading } = useQuery(getUsersFavouriteEducationsQuery(userId));
+  // const { data, isLoading } = useQuery(getUsersFavouriteEducationsQuery({ userId }));
+
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading, isError, error } = useInfiniteQuery({
+    queryKey: [queryKeys.usersFavouriteEducationsListQuery, userId],
+    queryFn: async ({ pageParam = 1 }) => {
+      const response = await window.electronAPI.getUsersFavouriteEducationsList(userId, {
+        page: pageParam,
+        limit: 10, // veya istediğiniz limit değeri
+      });
+      if (!response.success) {
+        throw response.error;
+      }
+      return response.data;
+    },
+    getNextPageParam: lastPage => {
+      // Eğer son sayfadaysak undefined döndür (daha fazla sayfa yok)
+      if (lastPage.pagination.page >= lastPage.pagination.totalPages) {
+        return undefined;
+      }
+      // Bir sonraki sayfa numarasını döndür
+      return lastPage.pagination.page + 1;
+    },
+    initialPageParam: 1, // İlk sayfa parametresi
+  });
+
+  if (!data) return null;
 
   return (
     <Tabs.Content value="user-favorite-educations">
-      <ul>
-        <pre>{JSON.stringify(isLoading, null, 4)}</pre>
-        {/* {data?.map(education => (
-          <EducationListItem key={education.id} education={education} />
-        ))} */}
-      </ul>
+      {data
+        ? data.pages.map(page => {
+            if (page.items.length === 0) {
+              return (
+                <span
+                  key={page.pagination.page}
+                  className="text-base-content/50 flex items-center justify-center py-4 pt-10"
+                >
+                  Bu kullanıcıya ait favori eğitim bulunmamaktadır.
+                </span>
+              );
+            }
+            return (
+              <ul
+                className="grid gap-2"
+                key={page.pagination.page}
+              >
+                {page.items.map(item => (
+                  <EducationListItem
+                    key={item.id}
+                    education={item}
+                  />
+                ))}
+              </ul>
+            );
+          })
+        : null}
     </Tabs.Content>
   );
 }
